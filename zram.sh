ad17fbf2e76b6ae520ec3ceb2100ad97dcd18fc9
Obsidian Maximus <obsidianmaximus@pm.me>
2024-04-15 13:18:12 +0530

    Remove comment
    Removing comment that lead to zram not getting activated.

--- zram.sh
+++ zram.sh
@@ -6,8 +6,13 @@ sudo apt install systemd-zram-generator #&& sudo nvim /etc/systemd/zram-generato
 
 echo "[zram0] 
 compression-algorithm = zstd
-zram-fraction = 1 # zram-size does not work anymore, so have this line instead.
-max-zram-size = 100000 # this has been done because by default, the script takes 4gb as max zram size, thus had to override it." | sudo tee /etc/systemd/zram-generator.conf
+zram-fraction = 1
+max-zram-size = 100000" | sudo tee /etc/systemd/zram-generator.conf
+
+#echo "[zram0] 
+#compression-algorithm = zstd
+#zram-fraction = 1 # zram-size does not work anymore, so have this line instead.
+#max-zram-size = 100000 # this has been done because by default, the script takes 4gb as max zram size, thus had to override it." | sudo tee /etc/systemd/zram-generator.conf
 
  
 
